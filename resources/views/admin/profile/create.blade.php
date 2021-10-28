10745684eb9d388607f49fbad6865973d87332c2
nakayama naho <forbussinesfighter@gmail.com>
2021-10-28 07:42:02 +0000

    Laravel11課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,24 @@
         <title>MyNews</title>
     </head>
     <body>
-        <h1>プロフィール作成画面</h1>
+        <h1>プロフィール</h1>
+        
+                {{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'Myプロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
     </body>
 </html>
